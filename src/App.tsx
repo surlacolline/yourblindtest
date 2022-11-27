20fbd9d6255e3b65d7b925233fed2afa954c5785
Elise <elise.juif@gsoft.com>
2022-11-27 09:46:49 -0500

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,38 @@
-import { useContext, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import {ConfigContext} from "./context/config-context";
-
+import { useContext, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { ConfigContext } from "./context/config-context";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [weather, setWeather] = useState(0)
-  const {configs, addConfig} = useContext(ConfigContext);
+  const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState("");
+  const { configs, addConfig } = useContext(ConfigContext);
 
-  useEffect(()=> {
+  useEffect(() => {
     const weatherurl = "https://localhost:49153/WeatherForecast";
     //const weatherurl = "https://yourblindtest-api.herokuapp.com/weatherforecast";
     fetch(weatherurl)
-    .then((response) => response.json())
-    .then((data) => {
-     setWeather(data);
-      console.log(data);
-    });
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const a = JSON.stringify(data);
+        setWeather(a);
+        console.log(a);
+      });
   }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React {configs.theme}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <span>{weather.toString()}</span>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <span>{weather}</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
